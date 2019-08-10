a8eb3b212920d0d43aecc6a1470ea70d3192b971
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-08-10 12:30:21 +0200

    Minor modifications to script to submit MC samples.

--- ggNtuplizer/test/submitMC.sh
+++ ggNtuplizer/test/submitMC.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-cd ~/private/stealth/cmssw/CMSSW_9_4_9_cand2/src/ && eval `scramv1 runtime -sh`
-cd ~/private/stealth/cmssw/CMSSW_9_4_9_cand2/src/ggAnalysis/ggNtuplizer/test
+BASEHOSTNAME=$(echo $HOSTNAME | sed "s|\([^\.]*\)\.cern\.ch|\1|" | sed "s|\([^\.]*\)\.fnal\.gov|\1|")
+NTUPLIZER_SRC=""
+export X509_USER_PROXY=${HOME}/private/x509up_u$(id -u)
+if [[ "${BASEHOSTNAME}" =~ ^lxplus[0-9]{3,4}$ ]]; then
+    NTUPLIZER_SRC="~/public/research/stealth/from_michael/StealthProduction/CMSSW_9_4_13/src"
+elif [[ "${BASEHOSTNAME}" =~ ^cmslpc[0-9]{2}$ ]]; then
+    NTUPLIZER_SRC="~/private/stealth/cmssw/CMSSW_9_4_13/src"
+fi
+cd ${NTUPLIZER_SRC} && eval `scramv1 runtime -sh`
+cd ${NTUPLIZER_SRC}/ggAnalysis/ggNtuplizer/test
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 
-set +x
+set -x
 
 # Comment out selection for submission
 crab submit --dryrun -c mc_crabConfig.py General.requestName=ntuplizer_949cand2_mc_Fall17_T5Wg General.workArea=crab_workArea_ntuplizer_949cand2_mc_Fall17_T5Wg Data.inputDataset=/SMS-T5WgStealth_TuneCP2_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PUFall17Fast_94X_mc2017_realistic_v15-v1/MINIAODSIM Data.outLFNDirBase=/store/group/lpcsusystealth/stealth2018Ntuples_with949Cand2/MC_Fall17_T5Wg_producedAug19/
@@ -18,3 +26,5 @@ crab submit --dryrun -c mc_crabConfig.py General.requestName=ntuplizer_949cand2_
 crab submit --dryrun -c mc_crabConfig.py General.requestName=ntuplizer_949cand2_mc_Fall17_EMEnrichedQCD_Pt-30toInf General.workArea=crab_workArea_ntuplizer_949cand2_mc_Fall17_EMEnrichedQCD_Pt-30toInf Data.inputDataset=/QCD_Pt-30toInf_DoubleEMEnriched_MGG-40to80_TuneCP5_13TeV_Pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM Data.outLFNDirBase=/store/group/lpcsusystealth/stealth2018Ntuples_with949Cand2/MC_Fall17_EMEnrichedQCD_Pt-30toInf_producedAug19/
 
 crab submit --dryrun -c mc_crabConfig.py General.requestName=ntuplizer_949cand2_mc_Fall17_EMEnrichedQCD_Pt-40toInf General.workArea=crab_workArea_ntuplizer_949cand2_mc_Fall17_EMEnrichedQCD_Pt-40toInf Data.inputDataset=/QCD_Pt-40toInf_DoubleEMEnriched_MGG-80toInf_TuneCP5_13TeV_Pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM Data.outLFNDirBase=/store/group/lpcsusystealth/stealth2018Ntuples_with949Cand2/MC_Fall17_EMEnrichedQCD_Pt-40toInf_producedAug19/
+
+set +x
