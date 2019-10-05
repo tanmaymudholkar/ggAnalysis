d96e3dc3a8c7949151185135a72f28a2cfc4311f
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-10-05 11:17:42 -0500

    Crab job submission for data moved to one script.

--- ggNtuplizer/test/data_crabConfig.py
+++ ggNtuplizer/test/data_crabConfig.py
@@ -6,11 +6,11 @@ config.General.workArea = ""
 config.General.transferLogs = False
 
 config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "run_data2017_94X.py"
+config.JobType.psetName = ""
 
 config.Data.inputDataset = ""
 config.Data.splitting = "LumiBased"
 config.Data.unitsPerJob = 15
-config.Data.lumiMask = "https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON.txt"
+config.Data.lumiMask = ""
 config.Data.outLFNDirBase = ""
-config.Site.storageSite = "T3_US_FNALLPC"
+config.Site.storageSite = ""
