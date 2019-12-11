d9e5c66efa161ebede5dbe978c32e0ebae35f5d6
Tanmay Mudholkar <tkmudholkar@gmail.com>
2019-12-10 23:06:03 -0600

    Added SinglePhoton dataset for 2017 and 2018, needed for single-good-photon control selection.

--- ggNtuplizer/test/submitCrabJobs.py
+++ ggNtuplizer/test/submitCrabJobs.py
@@ -55,14 +55,27 @@ datasets = {
         "/DoubleEG/Run2016E-17Jul2018-v1/MINIAOD",
         "/DoubleEG/Run2016F-17Jul2018-v1/MINIAOD",
         "/DoubleEG/Run2016G-17Jul2018-v1/MINIAOD",
-        "/DoubleEG/Run2016H-17Jul2018-v1/MINIAOD"
+        "/DoubleEG/Run2016H-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016B-17Jul2018_ver1-v1/MINIAOD",
+        "/SinglePhoton/Run2016B-17Jul2018_ver2-v1/MINIAOD",
+        "/SinglePhoton/Run2016C-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016D-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016E-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016F-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016G-17Jul2018-v1/MINIAOD",
+        "/SinglePhoton/Run2016H-17Jul2018-v1/MINIAOD"
     ],
     2017: [
         "/DoubleEG/Run2017B-31Mar2018-v1/MINIAOD",
         "/DoubleEG/Run2017C-31Mar2018-v1/MINIAOD",
         "/DoubleEG/Run2017D-31Mar2018-v1/MINIAOD",
         "/DoubleEG/Run2017E-31Mar2018-v1/MINIAOD",
-        "/DoubleEG/Run2017F-31Mar2018-v1/MINIAOD"
+        "/DoubleEG/Run2017F-31Mar2018-v1/MINIAOD",
+        "/SinglePhoton/Run2017B-31Mar2018-v1/MINIAOD",
+        "/SinglePhoton/Run2017C-31Mar2018-v1/MINIAOD",
+        "/SinglePhoton/Run2017D-31Mar2018-v1/MINIAOD",
+        "/SinglePhoton/Run2017E-31Mar2018-v1/MINIAOD",
+        "/SinglePhoton/Run2017F-31Mar2018-v1/MINIAOD"
     ],
     2018: [
         "/EGamma/Run2018A-17Sep2018-v2/MINIAOD",
