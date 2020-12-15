c1f2d5b19f5b7f7b7c04ff0dda172fda8478b79c
Tanmay Mudholkar <tkmudholkar@gmail.com>
2020-12-15 09:55:26 -0600

    Added new GJet datasets.

--- ggNtuplizer/test/getStatus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /cvmfs/cms.cern.ch/crab3/crab.sh
-
-for crabdir in `find . -mindepth 2 -maxdepth 2 -type d | grep "crab_workArea"`; do
-    echo "Found crabdir = ${crabdir}. Status:"
-    crab status -d ${crabdir}
-done
---
+++ ggNtuplizer/test/mc_crabConfig_2018.py
@@ -0,0 +1,16 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = ""
+config.General.workArea = ""
+config.General.transferLogs = False
+
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "run_mc2018_102X.py"
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = ""
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = ""
+config.Site.storageSite = "T3_US_FNALLPC"
